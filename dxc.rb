98df7d4e3e6e6ef3b3fa5d371997463add989425
Selim Sandal <49725809+selimsandal@users.noreply.github.com>
2025-01-06 16:39:39 +0300

    Update dxc.rb

--- dxc.rb
+++ dxc.rb
@@ -1,21 +1,22 @@
 class Dxc < Formula
-  desc "DirectX Shader Compiler"
-  homepage "https://github.com/microsoft/DirectXShaderCompiler"
-  license "LLVM"
-  head "https://github.com/microsoft/DirectXShaderCompiler.git", branch: "main"
+ desc "DirectX Shader Compiler"
+ homepage "https://github.com/microsoft/DirectXShaderCompiler"
+ license "LLVM" 
+ head "https://github.com/microsoft/DirectXShaderCompiler.git", branch: "main"
 
-  depends_on "cmake" => :build
-  depends_on "ninja" => :build
-  depends_on "python" => :build
+ depends_on "cmake" => :build
+ depends_on "ninja" => :build
+ depends_on "python" => :build
 
-  def install
-    system "cmake", "-S", ".", "-B", "build",
-           "-C", "cmake/caches/PredefinedParams.cmake",
-           "-DCMAKE_BUILD_TYPE=Release",
-           "-DCMAKE_INSTALL_PREFIX=#{prefix}",
-           "-GNinja"
-    
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
-  end
+ def install
+   system "cmake", "-S", ".", "-B", "build",
+          "-C", "cmake/caches/PredefinedParams.cmake", 
+          "-DCMAKE_BUILD_TYPE=Release",
+          "-GNinja"
+   
+   system "cmake", "--build", "build"
+
+   # Copy binaries
+   bin.install "build/bin/dxc"
+ end
 end
