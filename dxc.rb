2f32f6a39b9ba9c6672b46c0779398c0b46712a3
Selim Sandal <49725809+selimsandal@users.noreply.github.com>
2025-01-24 13:19:56 +0300

    Update dxc.rb

--- dxc.rb
+++ dxc.rb
@@ -1,31 +1,34 @@
 class Dxc < Formula
- desc "DirectX Shader Compiler"
- homepage "https://github.com/microsoft/DirectXShaderCompiler"
- license "LLVM" 
- head "https://github.com/microsoft/DirectXShaderCompiler.git", branch: "main"
+  desc "DirectX Shader Compiler"
+  homepage "https://github.com/microsoft/DirectXShaderCompiler"
+  license "LLVM"
+  head "https://github.com/microsoft/DirectXShaderCompiler.git", branch: "main"
 
- depends_on "cmake" => :build
- depends_on "ninja" => :build
- depends_on "python" => :build
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "python" => :build
 
- def install
-   system "cmake", "-S", ".", "-B", "build",
-          "-C", "cmake/caches/PredefinedParams.cmake", 
-          "-DCMAKE_BUILD_TYPE=Release",
-          "-GNinja"
-   
-   system "cmake", "--build", "build"
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+           "-C", "cmake/caches/PredefinedParams.cmake",
+           "-DCMAKE_BUILD_TYPE=Release",
+           "-GNinja"
+    
+    system "cmake", "--build", "build"
 
-   cd "build/bin" do
-     ["dxc", "dxa", "dxl", "dxopt", "dxr", "dxv"].each do |tool|
-       real_path = File.realpath(tool)
-       bin.install real_path => tool
-     end
-   end
+    cd "build/bin" do
+      ["dxc", "dxa", "dxl", "dxopt", "dxr", "dxv"].each do |tool|
+        real_path = File.realpath(tool)
+        bin.install real_path => tool
+      end
+    end
 
-   cd "build/lib" do
-     lib.install "libdxcompiler.so" if OS.linux?
-     lib.install "libdxcompiler.dylib" if OS.mac?
-   end
- end
+    cd "build/lib" do
+      lib.install "libdxcompiler.so" if OS.linux?
+      lib.install "libdxcompiler.dylib" if OS.mac?
+    end
+
+    # Copy the include directory
+    include.install Dir["include/*"]
+  end
 end
